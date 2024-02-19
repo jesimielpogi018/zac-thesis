54f19cbe1949b27564b62635afa7158f0dcc95bd
Yiell Chan <jesimielalmedejar1018@gmail.com>
2024-02-19 23:29:55 +0800

    remove the implementation of superforms

--- src/routes/table/+page.server.ts
+++ src/routes/table/+page.server.ts
@@ -1,9 +1,17 @@
 import type { PageServerLoad } from './$types';
 
+const availableEquipments = ['excavator', 'dump', 'roller', 'bulldozer', 'grader', 'backhoe'];
+
 export const load: PageServerLoad = async ({ url }) => {
+	const equipments: string[] = [];
+	let username: string = '';
+	let company: string = '';
+
 	for (const [key, value] of url.searchParams.entries()) {
-		console.log(key, value);
+		if (key === 'username') username = value;
+		if (key === 'company') company = value;
+		if (availableEquipments.includes(key)) equipments.push(key);
 	}
 
-	return {};
+	return { equipments, username, company };
 };
---
+++ src/routes/table/+page.svelte
@@ -0,0 +1,246 @@
+<script lang="ts">
+	import {
+		Button,
+		Radio,
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	interface TableData {
+		cost: number;
+		mileage: number;
+		lifespan: number;
+		isUsed: boolean;
+		yearsInOperation: number;
+		salvageValue: number;
+		depreciationRate: number;
+	}
+
+	const previewData: { [key: string]: TableData } = {};
+
+	let excavatorCostElement: HTMLInputElement;
+	let excavatorMileageElement: HTMLInputElement;
+	let excavatorLifespanElement: HTMLInputElement;
+	let excavatorIsUsedElement: HTMLInputElement;
+	let excavatorYearsInOperationElement: HTMLInputElement;
+	let excavatorSalvageValueElement: HTMLInputElement;
+	let excavatorDepreciationRateElement: HTMLInputElement;
+	let excavatorBtn: HTMLInputElement;
+</script>
+
+<svelte:head>
+	<title>Equipments Table</title>
+</svelte:head>
+
+<div class="mx-auto flex max-w-7xl flex-col items-center px-4 py-8 lg:py-16">
+	<h2 class="mb-4 text-xl font-bold text-neutral-900">Please complete the form.</h2>
+	<h2 class="mb-4 text-lg font-bold text-neutral-600">Username: {data.username}</h2>
+	<h2 class="mb-4 text-lg font-bold text-neutral-600">Company: {data.company}</h2>
+
+	<Table divClass="relative overflow-x-auto w-full">
+		<TableHead>
+			<TableHeadCell>Equipment</TableHeadCell>
+			<TableHeadCell>Cost of Equipment</TableHeadCell>
+			<TableHeadCell>Mileage</TableHeadCell>
+			<TableHeadCell>Lifespan</TableHeadCell>
+			<TableHeadCell>Is used?</TableHeadCell>
+			<TableHeadCell>Years in Operation</TableHeadCell>
+			<TableHeadCell>Salvage Value</TableHeadCell>
+			<TableHeadCell>Depreciation Rate</TableHeadCell>
+			<TableHeadCell>Action</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			{#if data.equipments.includes('excavator')}
+				<TableBodyRow>
+					<TableBodyCell>Excavator</TableBodyCell>
+					<TableBodyCell>
+						<input bind:this={excavatorCostElement} class="input border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input bind:this={excavatorMileageElement} class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input bind:this={excavatorLifespanElement} class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Radio name="example">No</Radio>
+						<Radio name="example" checked={true}>Yes</Radio>
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Button>Save</Button>
+					</TableBodyCell>
+				</TableBodyRow>
+			{/if}
+
+			{#if data.equipments.includes('dump')}
+				<TableBodyRow>
+					<TableBodyCell>Dump Truck</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Radio name="example">No</Radio>
+						<Radio name="example" checked={true}>Yes</Radio>
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Button>Save</Button>
+					</TableBodyCell>
+				</TableBodyRow>
+			{/if}
+
+			{#if data.equipments.includes('roller')}
+				<TableBodyRow>
+					<TableBodyCell>Roller/Compactor</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Radio name="example">No</Radio>
+						<Radio name="example" checked={true}>Yes</Radio>
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Button>Save</Button>
+					</TableBodyCell>
+				</TableBodyRow>
+			{/if}
+
+			{#if data.equipments.includes('backhoe')}
+				<TableBodyRow>
+					<TableBodyCell>Backhoe</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Radio name="example">No</Radio>
+						<Radio name="example" checked={true}>Yes</Radio>
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Button>Save</Button>
+					</TableBodyCell>
+				</TableBodyRow>
+			{/if}
+
+			{#if data.equipments.includes('bulldozer')}
+				<TableBodyRow>
+					<TableBodyCell>Bulldozer</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Radio name="example">No</Radio>
+						<Radio name="example" checked={true}>Yes</Radio>
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Button>Save</Button>
+					</TableBodyCell>
+				</TableBodyRow>
+			{/if}
+
+			{#if data.equipments.includes('grader')}
+				<TableBodyRow>
+					<TableBodyCell>Grader</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Radio name="example">No</Radio>
+						<Radio name="example" checked={true}>Yes</Radio>
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<input class="border-inherit bg-inherit" />
+					</TableBodyCell>
+					<TableBodyCell>
+						<Button>Save</Button>
+					</TableBodyCell>
+				</TableBodyRow>
+			{/if}
+		</TableBody>
+	</Table>
+</div>
