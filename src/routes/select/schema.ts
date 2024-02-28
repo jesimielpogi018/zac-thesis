f9616dac097c1667bdbc6a33dd60f20af8f364a6
Yiell Chan <jesimielalmedejar1018@gmail.com>
2024-02-28 21:10:00 +0800

    change input min limit and max limit for username and company and its description

--- src/routes/select/schema.ts
+++ src/routes/select/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const schema = z.object({
-	username: z.string().min(4, { message: 'Must be 5 or more characters long' }),
-	company: z.string().min(4, { message: 'Must be 5 or more characters long' }),
+	username: z.string().min(4, { message: 'Must be 4 or more characters long' }),
+	company: z.string().min(4, { message: 'Must be 4 or more characters long' }),
 	equipments: z.array(z.string())
 });
